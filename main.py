d45f9393e298fe6737fafc133635fbea66e42088
IbrahimKhanGH <iboo11khan@gmail.com>
2025-01-04 18:09:26 -0600

    idk

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import cv2
 
 def main():
-    video_capture = cv2.VideoCapture(0)
-    if not video_capture.isOpened():
+    webcam = cv2.VideoCapture(0)
+    if not webcam.isOpened():
         print("Error: Could not open video device.")
         return
 
+    count = 0
+
     while True:
-        ret, frame = video_capture.read()
+        ret, frame = webcam.read()
         if not ret:
             break
+    
+        cv2.imshow('FunCam', frame)
 
+        count += 1
+        print(count)
+
+        # Wait for 1ms and check if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('x'):
+            break
+
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            cv2.imwrite(f'frame_{count}.jpg', frame)
+
+
+    webcam.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
